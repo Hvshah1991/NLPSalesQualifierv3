0b1c9f009ca1b8223f93182c4b3ca2b055aaf5ca
Harsh Shah <114290664+Hvshah1991@users.noreply.github.com>
2023-12-25 16:46:22 -0500

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -18,11 +18,7 @@ from PIL import Image
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
 import os
-from dotenv import load_dotenv # Add
-
-load_dotenv() # Add
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 def ChatGPT(user_query):
     '''
